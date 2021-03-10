3b349ba59058cc331d9cc1ad4329e290d4a49f5e
Cort Breuer <cb732@cornell.edu>
2021-03-09 23:11:21 -0500

    Fill in equations and parameters

--- src/Control.jl
+++ src/Control.jl
@@ -17,7 +17,10 @@ function calculate_transcriptional_control_array(t::Float64,x::Array{Float64,1},
     n = problem["inducer_cooperativity_parameter"]
 
     # TODO: write u-varible function here 
-    # u_variable = ...
+    f_I = (I^n)/(I^n + KD^n)
+    W1 = exp(-E1/R*T_K)
+    W2 = exp(-E2/R*T_K)
+    u_variable = (W1 + (f * W2))/(1 + W1 + (f * W2))
 
     # return -
     return u_variable
--- src/Problem.jl
+++ src/Problem.jl
@@ -11,19 +11,23 @@ function generate_problem_dictionary(path_to_parameters_file::String)::Dict{Stri
         # setup the initial condition array -
         initial_condition_array = [
             0.0 ;   # 1 mRNA
-            0.0 ;   # TODO: gene concentration goes here -
+            5 ;   # TODO: gene concentration goes here - units: nM
             0.0 ;   # 3 I = we'll fill this in the execute script 
         ]
 
-
         # TODO: calculate the mRNA_degradation_constant 
-        # mRNA_degradation_constant = ....
+        mRNA_half_life_in_min = toml_dictionary["mRNA_half_life_in_min"]
+        mRNA_degradation_constant = log(2)*mRNA_half_life_in_min
 
         # TODO: VMAX for transcription -
-        # VMAX = ...
+        transcription_elongation_rate = toml_dictionary["transcription_elongation_rate"]
+        characteristic_transcript_length = toml_dictionary["characteristic_transcript_length"]
+        RNAPII_concentration = toml_dictionary["RNAPII_concentration"]
+        gene_length_in_nt = toml_dictionary["gene_length_in_nt"]
+        transcription_elongation_rate_constant = transcription_elongation_rate/characteristic_transcript_length
+        VMAX = transcription_elongation_rate_constant*characteristic_transcript_length*RNAPII_concentration/gene_length_in_nt
 
         # TODO: Stuff that I'm forgetting?
-        # ...
 
         # --- PUT STUFF INTO problem_dictionary ---- 
         problem_dictionary["transcription_time_constant"] = toml_dictionary["transcription_time_constant"]
@@ -33,11 +37,16 @@ function generate_problem_dictionary(path_to_parameters_file::String)::Dict{Stri
         problem_dictionary["inducer_dissociation_constant"] = toml_dictionary["inducer_dissociation_constant"]
         problem_dictionary["ideal_gas_constant_R"] = 0.008314 # kJ/mol-K
         problem_dictionary["temperature_K"] = (273.15+37)
-        problem_dictionary["initial_condition_array"] = initial_condition_array
+        problem_dictionary["initital_condition_array"] = initital_condition_array
+        problem_dictionary["mRNA_degradation_constant"] = mRNA_degradation_constant
+        problem_dictionary["maximum_transcription_velocity"] = VMAX
+        problem_dictionary["characteristic_transcript_length"] = toml_dictionary["characteristic_transcript_length"]
+        problem_dictionary["inducer_cooperativity_parameter"] = toml_dictionary["inducer_cooperativity_parameter"]
+        problem_dictionary["mRNA_half_life_in_min"] = toml_dictionary["mRNA_half_life_in_min"]
+        problem_dictionary["RNAPII_concentration"] = toml_dictionary["RNAPII_concentration"]
+        problem_dictionary["transcription_elongation_rate"] = toml_dictionary["transcription_elongation_rate"]
+        problem_dictionary["gene_length_in_nt"] = toml_dictionary["gene_length_in_nt"]
 
-        # TODO: If we want to use a value later e.g., VMAX or mRNA_degradation_constant you need to put them in the Dictionary
-        # ...
-        
         # return -
         return problem_dictionary
     catch error
